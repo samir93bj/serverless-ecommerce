edb061675580e696fba7b4d93aa0af543bb318f7
samir mahmud <samir93bj@gmail.com>
2023-04-07 03:47:11 -0300

    feat: add log in console to apiGateway and apiRequestId

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -3,20 +3,25 @@ import { Context } from "vm";
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
+	const lambdaRequestId = context.awsRequestId;
+	const apiRequestId = event.requestContext.requestId; 
+
+	console.log(`API Gateway RequestId: ${apiRequestId}`)
+	console.log(`Lambda RequestId: ${lambdaRequestId}`)
+
 	const method = event.httpMethod
+	if (event.resource === "/products") {
+		if (method === 'GET') {
+			console.log('GET')
 
- if (event.resource === "/products") {
-	if (method === 'GET') {
-		console.log('GET')
-
-		return {
-			statusCode: 200,
-			body: JSON.stringify({
-				message: 'Success GET Products - Ok'
-			})
+			return {
+				statusCode: 200,
+				body: JSON.stringify({
+					message: 'Success GET Products - Ok'
+				})
+			}
 		}
 	}
- }
 
  return {
 	statusCode: 400,

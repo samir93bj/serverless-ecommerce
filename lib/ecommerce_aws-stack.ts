639ef99e204678464b38c579b13fb4cc86dcd6ee
samir mahmud <samir93bj@gmail.com>
2023-04-07 02:23:57 -0300

    chore: add aws-sdk aws-lambda

--- lib/ecommerce_aws-stack.ts
+++ lib/ecommerce_aws-stack.ts
@@ -5,12 +5,6 @@ import { Construct } from 'constructs';
 export class EcommerceAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcommerceAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+		
   }
 }

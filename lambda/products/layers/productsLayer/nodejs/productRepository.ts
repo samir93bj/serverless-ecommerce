6f9c0e041d32e7e1a7e230db902868fe9a4ef9a5
samir mahmud <samir93bj@gmail.com>
2023-04-08 16:58:59 -0300

    feat: add service getProductId

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -25,4 +25,19 @@ export class ProductRespository {
 
     return data.Items as Product[];
   }
+
+  async getProductById (productId: string): Promise<Product> {
+    const data = await this.ddbClient.get({
+      TableName: this.productsDdb,
+      Key: {
+        id: productId
+      }
+    }).promise();
+
+    if (!data.Item) {
+      throw new Error('Product not found.');
+    }
+
+    return data.Item as Product;
+  }
 }

990e9cff2065b5dc5d00ae5412f045cb3274b89f
samir mahmud <samir93bj@gmail.com>
2023-04-08 16:39:44 -0300

    feat: initialize class ProductRepository

---
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -0,0 +1,20 @@
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { v4 as uuid } from 'uuid';
+
+export interface Product {
+  id: string;
+  productName: string;
+  code: string;
+  price: number;
+  model: string;
+}
+
+export class ProductRespository {
+  private ddbClient: DocumentClient;
+  private productsDdb: string;
+
+  constructor (ddbClient: DocumentClient, productsDdb: string) {
+    this.ddbClient = ddbClient;
+    this.productsDdb = productsDdb;
+  }
+}

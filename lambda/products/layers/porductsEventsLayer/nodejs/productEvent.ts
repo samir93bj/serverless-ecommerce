cc66dd6874d28241edbbf0c0867ee612b1f9794c
samir mahmud <samir93bj@gmail.com>
2023-04-10 12:28:30 -0300

    feat: add enums and interface events products

---
+++ lambda/products/layers/porductsEventsLayer/nodejs/productEvent.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+export enum ProductEventType {
+  CREATED = 'PRODUCT_CREATED',
+  UPDATED = 'PRODUCT_UPDATED',
+  DELETED = 'PRODUCT_DELETED'
+};
+
+export interface ProductEvent {
+  requestId: string;
+  eventType: ProductEventType;
+  productId: string;
+  productCode: string;
+  productPrice: number;
+  email: string;
+}

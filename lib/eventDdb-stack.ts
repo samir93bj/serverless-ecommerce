877133e5dff89fd30602fd16e612505042cda6f4
samir mahmud <samir93bj@gmail.com>
2023-04-09 21:52:46 -0300

    feat: initialize database events

---
+++ lib/eventDdb-stack.ts
@@ -0,0 +1,7 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export class EventsDdbStack extends cdk.Stack {
+
+};

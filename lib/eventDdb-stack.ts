05ee7b2923d5bcdad26f4002c823a568e593fc4f
samir mahmud <samir93bj@gmail.com>
2023-04-09 22:02:04 -0300

    feat: define schema to table events

--- lib/eventDdb-stack.ts
+++ lib/eventDdb-stack.ts
@@ -3,5 +3,26 @@ import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class EventsDdbStack extends cdk.Stack {
+  readonly table: dynamodb.Table;
 
+  constructor (scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.table = new dynamodb.Table(this, 'EventsDdb', {
+      tableName: 'events',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'pk',
+        type: dynamodb.AttributeType.STRING
+      },
+      sortKey: {
+        name: 'sk',
+        type: dynamodb.AttributeType.STRING
+      },
+      timeToLiveAttribute: 'ttl',
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1
+    });
+  }
 };

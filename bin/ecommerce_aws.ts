0077e7963640c476f431286cc3c7e75b7836e401
samir mahmud <samir93bj@gmail.com>
2023-04-10 18:20:16 -0300

    fix: solved fix import eventDdbStack

--- bin/ecommerce_aws.ts
+++ bin/ecommerce_aws.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { ProductAppStack } from '../lib/productsApp-stack';
 import { EcommerceApiStack } from '../lib/ecommerceApi-stack';
 import { ProductsAppLayersStack } from '../lib/productsApiLayers-stack';
-import { EventsDdbStack } from 'lib/eventDdb-stack';
+import { EventsDdbStack } from '../lib/eventDdb-stack';
 
 const app = new cdk.App();
 

ba31560edc59449182f7262fd68c7d0d8c063cdb
samir mahmud <samir93bj@gmail.com>
2023-04-10 15:07:35 -0300

    fix: solved fix config tsconfig.json

---
+++ lambda/products/productEventsFunction.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-absolute-path */
+import { Callback, Context } from 'aws-lambda';
+import { ProductEvent } from '/opt/nodejs/productsEventsLayer';
+
+export async function handler (event: ProductEvent, context: Context, callback: Callback): Promise<void> {
+
+};

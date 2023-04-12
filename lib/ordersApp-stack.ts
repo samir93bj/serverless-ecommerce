83defde6d803be47ea8130725af94f0a4ad4571f
samir mahmud <samir93bj@gmail.com>
2023-04-11 22:00:21 -0300

    feat[orders] Initialize stackProduct

---
+++ lib/ordersApp-stack.ts
@@ -0,0 +1,16 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+import { Construct } from 'constructs';
+
+interface OrdersAppStackProps extends cdk.StackProps {
+  productsDdb: dynamodb.Table
+}
+
+export class OrdersAppStack extends cdk.Stack {
+  constructor (scope: Construct, id: string, props: OrdersAppStackProps) {
+    super(scope, id, props);
+  }
+}

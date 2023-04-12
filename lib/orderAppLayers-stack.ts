7a3bb9c21a9baa1273eefd4ceb85e996af5e78b3
samir mahmud <samir93bj@gmail.com>
2023-04-11 21:38:19 -0300

    feat [orders]: add layer prders

---
+++ lib/orderAppLayers-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+
+export class OrdersAppLayersStack extends cdk.Stack {
+  constructor (scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+
+    const ordersLayer = new lambda.LayerVersion(this, 'OrdersLayer', {
+      code: lambda.Code.fromAsset('lambda/orders/layers/ordersLayer'),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
+      layerVersionName: 'OrdersLayer',
+      removalPolicy: cdk.RemovalPolicy.RETAIN
+    });
+
+    // eslint-disable-next-line no-new
+    new ssm.StringParameter(this, 'OrdersLayerVersionArn', {
+      parameterName: 'OrdersLayerVersionArn',
+      stringValue: ordersLayer.layerVersionArn
+    });
+  }
+}

fb6cfc0a3465d1e8e17b1ab1e470bde09ad3a06f
samir mahmud <samir93bj@gmail.com>
2023-04-12 12:14:39 -0300

    feat [orders]: create orders table dynamo db

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -12,5 +12,20 @@ interface OrdersAppStackProps extends cdk.StackProps {
 export class OrdersAppStack extends cdk.Stack {
   constructor (scope: Construct, id: string, props: OrdersAppStackProps) {
     super(scope, id, props);
+
+    const ordersDdb = new dynamodb.Table(this, 'OrdersDdb', {
+      tableName: 'orders',
+      partitionKey: {
+        name: 'pk',
+        type: dynamodb.AttributeType.STRING
+      },
+      sortKey: {
+        name: 'sk',
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1
+    });
   }
 }

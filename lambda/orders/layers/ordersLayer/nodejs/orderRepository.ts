1fd2e69233ee6a089c8d57c3f8257dcf5479a813
samir mahmud <samir93bj@gmail.com>
2023-04-12 21:37:08 -0300

    feat: add service getOrdersByEmail

--- lambda/orders/layers/ordersLayer/nodejs/orderRepository.ts
+++ lambda/orders/layers/ordersLayer/nodejs/orderRepository.ts
@@ -48,4 +48,16 @@ export class OrderRepository {
 
     return data.Items as Order[];
   }
+
+  async getOrdersByEmail (email: string): Promise<Order[]> {
+    const data = await this.ddbClient.query({
+      TableName: this.ordersDdb,
+      KeyConditionExpression: 'pk = :email',
+      ExpressionAttributeValues: {
+        ':email': email
+      }
+    }).promise();
+
+    return data.Items as Order[];
+  }
 }
